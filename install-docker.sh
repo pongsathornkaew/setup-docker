24872d53cd8295f32c374eaae8a3ee61456b173a
pongsathornkaew <pongsathorn.kaew@gmail.com>
2023-05-16 09:32:58 +0700

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+sudo apt update
+sudo apt-get install ca-certificates curl gnup
+
+#Add Docker’s official GPG key:
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+#Use the following command to set up the repository:
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#Update the apt package index:
+sudo apt-get update
+
+#Install Docker Engine, containerd, and Docker Compose.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify Docker installation
+sudo docker version
